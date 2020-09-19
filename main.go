ec93c7d8a0468da5b16a0977fd6c4cfdbae16f55
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-19 13:05:28 -0400

    adding configuration to main.go

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	Addr string
+}
+
 func main() {
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":5000", "HTTP network address")
+	flag.Parse()
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.LUTC|log.Llongfile)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC)
+
 	handler := http.HandlerFunc(Server)
-	if err := http.ListenAndServe(":5000", handler); err != nil {
-		log.Fatalf("could not listen on port 5000 %v", err)
+
+	srv := &http.Server{
+		Addr:    cfg.Addr,
+		Handler: handler,
 	}
+
+	infoLog.Printf("Starting server on %s", cfg.Addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
