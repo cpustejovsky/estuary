111fceef38dd4fd2b488306948ebe4c2860e6a24
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-20 12:22:44 -0400

    adding application struct to main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,11 @@ type Config struct {
 	Addr string
 }
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	cfg := new(Config)
 	flag.StringVar(&cfg.Addr, "addr", ":5000", "HTTP network address")
@@ -20,6 +25,11 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC)
 
 	handler := http.HandlerFunc(Server)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
 	srv := &http.Server{
 		Addr:    cfg.Addr,
