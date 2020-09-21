bab4df62cee2c940f291c80077ebec6769349281
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-21 09:21:30 -0400

    almost working with some regex and splitting

--- freewritechecker/freewritechecker.go
+++ freewritechecker/freewritechecker.go
@@ -1,5 +1,12 @@
 package freewritechecker
 
+import (
+	"regexp"
+)
+
 func WordCount(file []byte) (int, error) {
-	return 42, nil
+	re := regexp.MustCompile(`\s`)
+	text := string(file)
+	split := re.Split(text, -1)
+	return len(split), nil
 }
