09cb86baab1dd1b9d533bb271cd350266fea3e62
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-21 16:56:55 -0400

    fleshing out what routes will be needed for notes and projects

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,9 +9,25 @@ import (
 func (app *application) routes() http.Handler {
 	mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/", http.HandlerFunc(app.placeholder))
+
+	//Notes Routes
+	mux.Get("/api/notes/category/:name", http.HandlerFunc(app.getNote))
 	mux.Get("/api/notes/:id", http.HandlerFunc(app.getNote))
-	mux.Post("/api/notes", http.HandlerFunc(app.home))
+	mux.Post("/api/notes", http.HandlerFunc(app.placeholder))
+	mux.Patch("/api/notes/:id", http.HandlerFunc(app.placeholder))
+	mux.Patch("/api/notes/project", http.HandlerFunc(app.placeholder))
+	mux.Patch("/api/notes/:category", http.HandlerFunc(app.placeholder))
+	mux.Del("/api/notes/:id", http.HandlerFunc(app.placeholder))
+
+	//Project Routes
+	mux.Get("/api/projects/", http.HandlerFunc(app.placeholder))
+	mux.Get("/api/projects/done", http.HandlerFunc(app.placeholder))
+	mux.Get("/api/projects/show/:id", http.HandlerFunc(app.placeholder))
+	mux.Post("/api/projects", http.HandlerFunc(app.placeholder))
+	mux.Patch("/api/projects", http.HandlerFunc(app.placeholder))
+	mux.Patch("/api/projects/done", http.HandlerFunc(app.placeholder))
+	mux.Del("/api/projects/:id", http.HandlerFunc(app.placeholder))
 
 	return mux
 }
