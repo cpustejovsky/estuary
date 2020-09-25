11392f86a9914635a8341bc29c2b5e2d9bdb6654
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-25 15:11:57 -0400

    adding sql script to keep track of it (will find better place for it)

---
+++ pkg/models/users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id uuid DEFAULT uuid_generate_v4 (),
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL UNIQUE,
+    hashed_password CHAR(60) NOT NULL,
+    created timestamp without time zone default (now() at time zone 'utc'),
+    updated timestamp,
+    deleted timestamp,
+    email_updates BOOLEAN DEFAULT false,
+    advanced_view BOOLEAN DEFAULT false,
+    active BOOLEAN DEFAULT true,
+    PRIMARY KEY (id)
+);
