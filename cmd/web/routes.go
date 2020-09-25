2284c61aae5f7dec0ee739f13d6721d62db8b300
cpustejovsky <charles.pustejovsky@gmail.com>
2020-09-25 19:32:25 -0400

    setting up middleware in routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,40 +4,44 @@ import (
 	"net/http"
 
 	"github.com/bmizerany/pat"
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	dynamicMiddleware := alice.New(app.session.Enable, noSurf, app.authenticate)
+
 	mux := pat.New()
 
 	//Auth Routes
-	mux.Post("/api/signup", http.HandlerFunc(app.signup))
-	mux.Post("/api/login", http.HandlerFunc(app.login))
-	mux.Get("/api/logout", http.HandlerFunc(app.placeholder))
-	mux.Get("/auth/google", http.HandlerFunc(app.placeholder))
-	mux.Get("/auth/google/callback", http.HandlerFunc(app.placeholder))
+	mux.Post("/api/signup", dynamicMiddleware.ThenFunc(app.signup))
+	mux.Post("/api/login", dynamicMiddleware.ThenFunc(app.login))
+	mux.Get("/api/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Get("/auth/google", dynamicMiddleware.ThenFunc(app.placeholder))
+	mux.Get("/auth/google/callback", dynamicMiddleware.ThenFunc(app.placeholder))
 
 	//User Routes
-	mux.Get("/api/user", http.HandlerFunc(app.getUser))
-	mux.Patch("/api/user", http.HandlerFunc(app.placeholder))
-	mux.Del("/api/user", http.HandlerFunc(app.placeholder))
+	mux.Get("/api/user", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.getUser))
+	mux.Patch("/api/user", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Del("/api/user", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
 
 	//Notes Routes
-	mux.Get("/api/notes/category/:name", http.HandlerFunc(app.getNote))
-	mux.Get("/api/notes/:id", http.HandlerFunc(app.getNote))
-	mux.Post("/api/notes", http.HandlerFunc(app.placeholder))
-	mux.Patch("/api/notes/:id", http.HandlerFunc(app.placeholder))
-	mux.Patch("/api/notes/project", http.HandlerFunc(app.placeholder))
-	mux.Patch("/api/notes/:category", http.HandlerFunc(app.placeholder))
-	mux.Del("/api/notes/:id", http.HandlerFunc(app.placeholder))
+	mux.Get("/api/notes/category/:name", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.getNote))
+	mux.Get("/api/notes/:id", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.getNote))
+	mux.Post("/api/notes", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Patch("/api/notes/:id", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Patch("/api/notes/project", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Patch("/api/notes/:category", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Del("/api/notes/:id", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
 
 	//Project Routes
-	mux.Get("/api/projects/", http.HandlerFunc(app.placeholder))
-	mux.Get("/api/projects/done", http.HandlerFunc(app.placeholder))
-	mux.Get("/api/projects/show/:id", http.HandlerFunc(app.placeholder))
-	mux.Post("/api/projects", http.HandlerFunc(app.placeholder))
-	mux.Patch("/api/projects", http.HandlerFunc(app.placeholder))
-	mux.Patch("/api/projects/done", http.HandlerFunc(app.placeholder))
-	mux.Del("/api/projects/:id", http.HandlerFunc(app.placeholder))
+	mux.Get("/api/projects/", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Get("/api/projects/done", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Get("/api/projects/show/:id", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Post("/api/projects", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Patch("/api/projects", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Patch("/api/projects/done", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
+	mux.Del("/api/projects/:id", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.placeholder))
 
-	return mux
+	return standardMiddleware.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/jinzhu/gorm v1.9.16
 	github.com/joho/godotenv v1.3.0
+	github.com/justinas/alice v1.2.0
 	github.com/justinas/nosurf v1.1.1
 	github.com/lib/pq v1.8.0
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
